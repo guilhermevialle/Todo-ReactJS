920685326b5ee15a2570268d35639cc5773b2332
guilhermevialle <guilhef2@hotmail.com>
2021-11-05 13:45:35 -0300

    making newlist component yet, adding colors select

--- src/components/newlist/newlist.js
+++ src/components/newlist/newlist.js
@@ -1,8 +1,73 @@
 import "./newlist.scss";
 import Tab from "../tab/tab";
+import listSvg from "../currentlist/svgs/list-svg.svg";
+import { useState, useEffect } from "react";
 
 export default () => {
-    <div className="newlist">
-        <Tab left="Cancelar" middle="Nova Lista" right="OK" />
-    </div>;
+    const [listObject, setListObject] = useState({
+        color: "#00c7bd",
+    });
+
+    const colorScheme = [
+        "#ff3a30",
+        "#ff9500",
+        "#ffcc00",
+        "#34c759",
+        "#00c7bd",
+        "#af52de",
+        "#a2855e",
+    ];
+
+    const eventHandler = (evt) => {
+        const target = evt.currentTarget;
+        const value = target.value;
+
+        if (evt.keyCode == "13") {
+            setListObject((prev) => {
+                prev.listName = value;
+                return prev;
+            });
+        }
+    };
+
+    return (
+        <div className="newlist">
+            <Tab left="Cancelar" middle="Nova Lista" right="OK" />
+            <div className="iconarea">
+                <div
+                    style={{ backgroundColor: listObject.color }}
+                    className="icon-select"
+                >
+                    <img src={listSvg} />
+                </div>
+            </div>
+            <div className="inparea">
+                <input
+                    style={{ color: listObject.color }}
+                    type="text"
+                    onKeyUp={(e) => {
+                        eventHandler(e);
+                    }}
+                />
+            </div>
+            <div className="colorselect">
+                {colorScheme.map((c) => {
+                    return (
+                        <div
+                            style={{ backgroundColor: c }}
+                            className="colorTemplate"
+                            onClick={() => {
+                                console.log(listObject);
+
+                                setListObject((prev) => {
+                                    prev.color = c;
+                                    return prev;
+                                });
+                            }}
+                        ></div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 };
