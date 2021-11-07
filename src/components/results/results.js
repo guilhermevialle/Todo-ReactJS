458b89dbce8decf33842c7261c7290317a7f4268
guilhermevialle <guilhef2@hotmail.com>
2021-11-07 18:35:43 -0300

    adding input search functionally

--- src/components/results/results.js
+++ src/components/results/results.js
@@ -3,17 +3,33 @@ import "./results.scss";
 import { useContext, useEffect, useState } from "react";
 import CurrentList from "../currentlist/currentlist";
 import { Global } from "../../App";
+import listslider from "../listslider/listslider";
 
 export default () => {
-    let { data } = useContext(Global);
+    let { data, setData } = useContext(Global);
     const { listState, searchinp } = data;
-    const [x, setX] = useState(null);
 
     useEffect(() => {
-        setX(listState.filter((l) => l.listName.includes(searchinp) != -1));
+        const res = document.querySelectorAll(
+            ".results .currentlist .lside span"
+        );
+
+        for (let x = 0; x < res.length; x++) {
+            let txtValue = res[x].innerText || res[x].textContent;
+
+            if (txtValue.toUpperCase().indexOf(searchinp) > -1) {
+                document.querySelectorAll(".results .currentlist")[
+                    x
+                ].style.display = "";
+            } else {
+                document.querySelectorAll(".results .currentlist")[
+                    x
+                ].style.display = "none";
+            }
+        }
     }, [searchinp]);
 
-    return x ? (
+    return listState ? (
         <div className="results">
             {listState.map((list, index) => {
                 return <CurrentList list={list} index={index} />;
